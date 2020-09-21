b44fd21218d9149f9c1471f564d5e2d5a045ce52
dev1980 <brham1980@gmail.com>
2020-09-21 23:10:39 +0545

    use api to fetch data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import ContactCard from './ContactCard';
 
+
 const App = () => {
+
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=10")
+    .then(response => response.json())
+    .then(data =>{
+      console.log(data)
+      setResults(data.results)
+    });
+}, [])
+
+return (
+  <div>
+{results.map((result, index)=> {
   return(
-    <div>
-    <ContactCard 
-    avatarUrl = 'https://via.placeholder.com/150'
-    name = "Jenny Han" email="jenny@gmail.com" age= {25} />
-    <ContactCard
-      avatarUrl = 'https://via.placeholder.com/150'
-    name = "Paul john" email="paul@gmail.com" age= {40} />
-     />
-    <ContactCard 
-        avatarUrl = 'https://via.placeholder.com/150'
-    name = "Peter jan" email="peter@gmail.com" age= {45} />
+    <ContactCard key={index}
+      avatarUrl = {result.picture.large}
+      name = {result.name.first}
+      email = {result.email}
+      age = {result.dob.age}
     />
-    </div>
   )
-}
+})}
+
+  </div>
+);
+};
 
 export default App;
 
